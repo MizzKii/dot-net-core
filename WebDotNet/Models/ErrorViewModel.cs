b57aeb430b3b40c029e7867c6c8c6b78632ab96d
MizzKii <MizzKiiPC@gmail.com>
2018-10-14 23:56:48 +0700

    first commit

---
+++ WebDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
